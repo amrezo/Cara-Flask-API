3673bf5d03821fd0ce43c45c57ba0a9a5d02b85c
Amr Al-Refae <amrezo94@gmail.com>
2023-06-20 12:31:08 +0300

    fix request issue

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def generate_reply():
     endpoint_name = 'jumpstart-dft-hf-llm-falcon-40b-instruct-bf16'
 
     client = boto3.client('runtime.sagemaker')
-    response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=request)
+    response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=request.get_json())
     model_predictions = json.loads(response['Body'].read())
     generated_text = model_predictions[0]['generated_text']
 
