a6738680be568af8b82597cd38f896b97d07d0c6
Amr Al-Refae <amrezo94@gmail.com>
2023-06-20 15:36:04 +0300

    fix request issue

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ def generate_reply():
     response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=request.data)
 
     model_predictions = json.loads(response['Body'].read())
-    generated_text = model_predictions[0]
+    generated_text = model_predictions[0]['generated_text']
 
     return generated_text
