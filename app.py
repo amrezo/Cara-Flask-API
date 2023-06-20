95d706c2b4138b9c79169ab3e418c733468506df
Amr Al-Refae <amrezo94@gmail.com>
2023-06-20 13:02:17 +0300

    fix request issue

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def generate_reply():
     endpoint_name = 'jumpstart-dft-hf-llm-falcon-40b-instruct-bf16'
 
     client = boto3.client('runtime.sagemaker')
-    response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=json.dumps(request.text).encode('utf-8'))
+    response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=json.dumps(request).encode('utf-8'))
 
     return response
