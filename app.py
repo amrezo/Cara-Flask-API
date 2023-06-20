c73dd08521cfc14c9d0b0b3e21a7ac575a601c98
Amr Al-Refae <amrezo94@gmail.com>
2023-06-20 12:11:06 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import json
+import boto3
+
+
+app = Flask(__name__)
+
+@app.route('/api/v1/generate_reply', methods=['POST'])
+def generate_reply():
+    endpoint_name = 'jumpstart-dft-hf-llm-falcon-40b-instruct-bf16'
+
+    client = boto3.client('runtime.sagemaker')
+    response = client.invoke_endpoint(EndpointName=endpoint_name, ContentType='application/json', Body=json.dumps(request).encode('utf-8'))
+    model_predictions = json.loads(response['Body'].read())
+    generated_text = model_predictions[0]['generated_text']
+
+    return response
